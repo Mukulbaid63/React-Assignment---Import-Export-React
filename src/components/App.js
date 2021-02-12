dec175dcde674d236736a74ffb479b9bef378579
Muddhit Baid <61585314+Mukulbaid63@users.noreply.github.com>
2021-02-13 02:21:14 +0545

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-
+global.TimerSDK = require('path-to-script'); 
 const App = () => {
   return (
     <div id="main">
